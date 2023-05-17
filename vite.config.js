432de34fc584c51eae8ef6db0665a61ff500d17d
Chohyejin53 <hyejin2338@gmail.com>
2023-05-17 20:28:42 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   //github 저장소 이름
-  base: '/ttt-12'
+  // base: '/ttt-12'
+  base: isProduction ? '/ttt-12' : '/'
+
 })
